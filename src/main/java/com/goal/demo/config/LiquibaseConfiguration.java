e31e10770f6002dccdc690f9fd198c035970cdbc
Regina Borgno <reginaborgno013@gmail.com>
2024-08-01 19:27:29 -0300

    Setup configuration, dependencies, properties and containerized db

---
+++ src/main/java/com/goal/demo/config/LiquibaseConfiguration.java
@@ -0,0 +1,70 @@
+package com.goal.demo.config;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseDataSource;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
+
+import javax.sql.DataSource;
+import java.util.concurrent.Executor;
+
+@Configuration
+public class LiquibaseConfiguration {
+
+    private final Logger log = LoggerFactory.getLogger(LiquibaseConfiguration.class);
+
+    private final Environment env;
+
+    public LiquibaseConfiguration(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public SpringLiquibase liquibase(
+        @Qualifier("taskExecutor") Executor executor,
+        LiquibaseProperties liquibaseProperties,
+        @LiquibaseDataSource ObjectProvider<DataSource> liquibaseDataSource,
+        ObjectProvider<DataSource> dataSource,
+        DataSourceProperties dataSourceProperties
+    ) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        DataSource liquibaseDataSourceInstance = liquibaseDataSource.getIfAvailable();
+
+        if (liquibaseDataSourceInstance != null) {
+            liquibase.setDataSource(liquibaseDataSourceInstance);
+        } else {
+            liquibase.setDataSource(dataSource.getIfUnique());
+        }
+
+        liquibase.setChangeLog("classpath:config/liquibase/master.xml");
+        liquibase.setContexts(liquibaseProperties.getContexts());
+        liquibase.setDefaultSchema(liquibaseProperties.getDefaultSchema());
+        liquibase.setLiquibaseSchema(liquibaseProperties.getLiquibaseSchema());
+        liquibase.setLiquibaseTablespace(liquibaseProperties.getLiquibaseTablespace());
+        liquibase.setDatabaseChangeLogLockTable(liquibaseProperties.getDatabaseChangeLogLockTable());
+        liquibase.setDatabaseChangeLogTable(liquibaseProperties.getDatabaseChangeLogTable());
+        liquibase.setDropFirst(liquibaseProperties.isDropFirst());
+        liquibase.setLabelFilter(liquibaseProperties.getLabelFilter());
+        liquibase.setChangeLogParameters(liquibaseProperties.getParameters());
+        liquibase.setRollbackFile(liquibaseProperties.getRollbackFile());
+        liquibase.setTestRollbackOnUpdate(liquibaseProperties.isTestRollbackOnUpdate());
+        liquibase.setShouldRun(liquibaseProperties.isEnabled());
+
+        if (env.acceptsProfiles(Profiles.of("no-liquibase"))) {
+            liquibase.setShouldRun(false);
+        } else {
+            liquibase.setShouldRun(liquibaseProperties.isEnabled());
+            log.debug("Configuring Liquibase");
+        }
+
+        return liquibase;
+    }
+}
